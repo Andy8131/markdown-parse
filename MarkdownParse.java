b9df638f5879cd9c7d82862cbd0591d1cde0fe96
Andy8131 <andy.liu0123456@gmail.com>
2022-01-19 18:52:59 -0800

    creates forever loop by removing parentheses at the end

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,9 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1;  
+
+            System.out.println(currentIndex);
         }
         return toReturn;
     }
