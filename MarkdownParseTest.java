ecc967ec74de5965727ce89b93bce2cfc528893e
Andy8131 <andy.liu0123456@gmail.com>
2022-02-25 01:08:09 -0800

    added testfiles and tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,44 +3,32 @@ import org.junit.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
 
 public class MarkdownParseTest {
 
-    @Before
-    public void setUp() throws Exception {
-        ArrayList<String> lists = MarkdownParse.getLinks(Files.readString(Path.of("test-file1.md")));
+    @Test
+    public void testFiler1() throws IOException{
+        String contents= Files.readString(Path.of("./test-filer1.md"));
+        List<String> expect = List.of("another link`","cod[e","code]");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
-
 
     @Test
-    public void addition() {
-        assertEquals(3, 1 + 1);
-    }
-    
-    // @Test
-    // public void readFile(){
-    //     // ArrayList<String> result = MarkdownParse.getLinks(Files.readString(Path.of("test-file1.md")));
-    //     try{
-    //         String toReturn = Files.readString(Path.of("./test-file.md"));
-    //         fail();
-    //     }catch(Exception e){
-    //         //pass
-    //     }
-    // }
+    public void testFiler2() throws IOException{
+        String contents= Files.readString(Path.of("./test-filer1.md"));
+        List<String> expect = List.of("nested link","a nested parenthesized url",
+        "some escaped [ brackets ]");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }    
 
-        // @Test
-        // public void test1()throws IOException{
-        //     Path fileName = Path.of("test-file1.md");
-        //     String contents = Files.readString(fileName);
-        //     ArrayList<String> links = MarkdownParse.getLinks(contents);
-        //     System.out.println(links);
-        //     // [https://something.com, some-page.html]
-        //     String expected =  "[https://something.com, some-page.html]";
-        //     assertEquals(expected, links);
-        // }
+    @Test
+    public void testFiler3() throws IOException{
+        String contents= Files.readString(Path.of("./test-filer1.md"));
+        List<String> expect = List.of("this title text is really long and takes up more than one line");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+
 }
