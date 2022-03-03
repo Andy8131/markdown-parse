d18034699fdd11f9f70bb8aa49c84d356ba195a2
Andy8131 <andy.liu0123456@gmail.com>
2022-03-02 19:25:38 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,14 +17,14 @@ public class MarkdownParseTest {
 
     @Test
     public void testFiler2() throws IOException{
-        String contents= Files.readString(Path.of("./test-filer1.md"));
+        String contents= Files.readString(Path.of("./test-filer2.md"));
         List<String> expect = List.of("a.com","a.com(())","example.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }    
 
     @Test
     public void testFiler3() throws IOException{
-        String contents= Files.readString(Path.of("./test-filer1.md"));
+        String contents= Files.readString(Path.of("./test-filer3.md"));
         List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
